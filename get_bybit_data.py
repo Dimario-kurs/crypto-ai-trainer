8920aa3b780c101f8f5d2b2bb19317c71c25eb8e
Dimario-kurs <ggrok92@gmail.com>
2025-09-02 20:47:42 +0700

    Update get_bybit_data.py

--- get_bybit_data.py
+++ get_bybit_data.py
@@ -6,22 +6,30 @@ from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv("BYBIT_API_KEY")
 secret_key = os.getenv("BYBIT_SECRET_KEY")
+proxy = os.getenv("PROXY")
 
 if not api_key or not secret_key:
-    raise ValueError("❌ Ключи API не найдены! Проверь файл .env")
+    raise ValueError("❌ Ключи API не найдены! Проверь .env")
 
-# === Подключаемся к Bybit ===
+# === Подключаемся к Bybit с прокси ===
 exchange = ccxt.bybit({
     "apiKey": api_key,
     "secret": secret_key,
     "enableRateLimit": True,
-    "options": {"defaultType": "spot"}  # можно 'linear' для фьючей
+    "params": {"defaultType": "spot"},
 })
+
+# Если задан прокси, добавляем его
+if proxy:
+    exchange.proxies = {
+        "http": proxy,
+        "https": proxy,
+    }
 
 # === Проверка соединения (баланс) ===
 try:
     balance = exchange.fetch_balance()
-    print("✅ Подключение успешно!")
+    print("✅ Успешное подключение!")
     print("Баланс:", balance["total"])
 except Exception as e:
     print("❌ Ошибка подключения:", str(e))
